dd51e6b364bc60fc94d92c878fbdce08835d494b
Jacek Wolnicki <wolnicki.jacek@gmail.com>
2017-05-13 12:33:05 +0200

    Dodanie funkcji Dodaj plac zabaw do przycisku fab (usunięcie błędu).

--- app/src/main/java/com/tpanpm/wwsis/placzabaw/AddPlayground.java
+++ app/src/main/java/com/tpanpm/wwsis/placzabaw/AddPlayground.java
@@ -1,19 +1,13 @@
 package com.tpanpm.wwsis.placzabaw;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
-import android.view.View;
-import android.widget.EditText;
+import android.os.Bundle;
 
-/**
- * Created by wolni on 13.05.2017.
- */
+public class AddPlayground extends AppCompatActivity {
 
-class AddPlayground extends AppCompatActivity{
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_playground);
     }
 }
