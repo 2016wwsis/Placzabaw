6357dbb9d8f161625d41960d56cee2870e8a55b1
Jacek Wolnicki <wolnicki.jacek@gmail.com>
2017-05-25 13:52:48 +0200

    Zwrócenie zdjęcia do przycisku (w trakcie...)

--- app/src/main/java/com/tpanpm/wwsis/placzabaw/AddPlayground.java
+++ app/src/main/java/com/tpanpm/wwsis/placzabaw/AddPlayground.java
@@ -1,44 +1,65 @@
 package com.tpanpm.wwsis.placzabaw;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
-import static com.tpanpm.wwsis.placzabaw.MainActivity.REQUEST_IMAGE_CAPTURE;
+// import static com.tpanpm.wwsis.placzabaw.MainActivity.REQUEST_IMAGE_CAPTURE;
 
 public class AddPlayground extends AppCompatActivity {
 
     EditText playGroundName;
     RatingBar rate;
+    ImageButton imageButton;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        setContentView(R.layout.activity_add_playground);
-        playGroundName = (EditText)findViewById(R.id.editText_playground_name);
-        rate = (RatingBar)findViewById(R.id.ratingBar);
 
-       Double message = intent.getDoubleExtra(MainActivity.EXTRA_MESSAGE, 0);
+        setContentView(R.layout.activity_add_playground);
+        playGroundName = (EditText) findViewById(R.id.editText_playground_name);
+        rate = (RatingBar) findViewById(R.id.ratingBar);
+        imageButton = (ImageButton) findViewById(R.id.imageButton_add_playground_image);
+
+        Intent intent = getIntent();
+        Double message = intent.getDoubleExtra(MainActivity.EXTRA_MESSAGE, 0);
+
         Toast.makeText(this, String.valueOf(message), Toast.LENGTH_SHORT).show();
     }
 
-    public void addPhoto(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-    }
-}
+        public void addPhoto (View view){
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
 
-    public void addNewPlayground(View view) {
-        Playground playground = new Playground(42.0, 17.0,
-               String.valueOf(playGroundName.getText()),
-                "ghvhg",
-                rate.getRating());
+            if (extras != null) {
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imageButton.setImageBitmap(imageBitmap);
+            }
+        }
     }
+
+        public void addNewPlayground (View view){
+            Playground playground = new Playground(42.0, 17.0,
+                    String.valueOf(playGroundName.getText()),
+                    "ghvhg",
+                    rate.getRating());
+        }
 }
