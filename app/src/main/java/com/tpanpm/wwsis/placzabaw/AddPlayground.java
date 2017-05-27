c7a7883970531ae009efeae289f341f4c83397f4
Robert <robertman84@gmail.com>
2017-05-27 16:32:57 +0200

    Dodanie robieania zdjęć Robert

--- app/src/main/java/com/tpanpm/wwsis/placzabaw/AddPlayground.java
+++ app/src/main/java/com/tpanpm/wwsis/placzabaw/AddPlayground.java
@@ -1,25 +1,160 @@
 package com.tpanpm.wwsis.placzabaw;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RatingBar;
-import android.widget.Toast;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.content.pm.PackageManager;
+    import android.content.pm.ResolveInfo;
+    import android.graphics.Bitmap;
+    import android.graphics.BitmapFactory;
+    import android.net.Uri;
+    import android.os.Build;
+    import android.os.Bundle;
+    import android.os.Environment;
+    import android.provider.MediaStore;
+    import android.support.v7.app.AppCompatActivity;
+    import android.util.Log;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.ImageButton;
+    import android.widget.RatingBar;
+    import android.widget.Toast;
+
+    import java.io.File;
+    import java.io.IOException;
+    import java.text.SimpleDateFormat;
+    import java.util.Date;
+    import java.util.List;
+
+    import static com.tpanpm.wwsis.placzabaw.MainActivity.REQUEST_IMAGE_CAPTURE;
 
 // import static com.tpanpm.wwsis.placzabaw.MainActivity.REQUEST_IMAGE_CAPTURE;
 
 public class AddPlayground extends AppCompatActivity {
 
-    EditText playGroundName, playGroundComment;
+    EditText playGroundName;
     RatingBar rate;
     ImageButton imageButton;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int ACTION_TAKE_PHOTO_B = 1;
+    private static final int ACTION_TAKE_PHOTO_S = 2;
+    private static final String BITMAP_STORAGE_KEY = "viewbitmap";
+    private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
+    private ImageButton mImageView;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+    private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
+    String imageFileName;
+    /* Photo album for this application */
+    private File getAlbumDir() {
+        File storageDir = null;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            storageDir = mAlbumStorageDirFactory.getAlbumStorageDir("Plac_zabaw");
+            if (storageDir != null) {
+                if (! storageDir.mkdirs()) {
+                    if (! storageDir.exists()){
+                        Log.d("CameraSample", "failed to create directory");
+                        return null;
+                    }
+                }
+            }
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+        return storageDir;
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        imageFileName = JPEG_FILE_PREFIX;
+        File albumF = getAlbumDir();
+        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+        //  Toast.makeText(this,
+        //        imageFileName + "\n"
+        //       + imageF.getName()
+        //      +"\n"+albumF,
+        //     Toast.LENGTH_LONG).show();
+        imageFileName=imageF.getName();
+        return imageF;
+    }
+    private File setUpPhotoFile() throws IOException {
+        File f = createImageFile();
+        mCurrentPhotoPath = f.getAbsolutePath();
+        // po createImageFile Toast.makeText(this, f.getName(), Toast.LENGTH_SHORT).show();
+        return f;
+    }
+    private void setPic() {
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+        /* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+        /* Decode the JPEG file into a Bitmap */
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        /* Associate the Bitmap to the ImageView */
+        mImageView.setImageBitmap(bitmap);
+    }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File f = new File(mCurrentPhotoPath);
+        //po zdjeciu
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        switch(actionCode) {
+            case ACTION_TAKE_PHOTO_B:
+                File f = null;
+                try {
+                    f = setUpPhotoFile();
+                    mCurrentPhotoPath = f.getAbsolutePath();
+                    //Toast.makeText(this, f.getName()+"\n hello", Toast.LENGTH_SHORT).show();
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    f = null;
+                    mCurrentPhotoPath = null;
+                }
+                break;
+            default:
+                break;
+        } // switch
+        startActivityForResult(takePictureIntent, actionCode);
+    }
+    private void handleSmallCameraPhoto(Intent intent) {
+        Bundle extras = intent.getExtras();
+        mImageBitmap = (Bitmap) extras.get("data");
+        mImageView.setImageBitmap(mImageBitmap);
+    }
+    private void handleBigCameraPhoto() {
+        if (mCurrentPhotoPath != null) {
+            setPic();
+            galleryAddPic();
+            mCurrentPhotoPath = null;
+        }
+    }
+
+    Button.OnClickListener mTakePicOnClickListener =
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,43 +163,93 @@ public class AddPlayground extends AppCompatActivity {
         setContentView(R.layout.activity_add_playground);
         playGroundName = (EditText) findViewById(R.id.editText_playground_name);
         rate = (RatingBar) findViewById(R.id.ratingBar);
-        imageButton = (ImageButton) findViewById(R.id.imageButton_add_playground_image);
-        playGroundComment = (EditText) findViewById(R.id.editText_playground_comment);
+        mImageView = (ImageButton) findViewById(R.id.imageButton_add_playground_image);
+        mImageBitmap = null;
+        ImageButton picBtn = mImageView;
+        setBtnListenerOrDisable(
+                picBtn,
+                mTakePicOnClickListener,
+                MediaStore.ACTION_IMAGE_CAPTURE
+        );
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
+            mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
+        } else {
+            mAlbumStorageDirFactory = new BaseAlbumDirFactory();
+        }
 
         Intent intent = getIntent();
-        Double message1 = intent.getDoubleExtra(MainActivity.EXTRA_MESSAGE1, 0);
-        Double message2 = intent.getDoubleExtra(MainActivity.EXTRA_MESSAGE2, 0);
-        Toast.makeText(this, String.valueOf(message1)+" "+String.valueOf(message2), Toast.LENGTH_SHORT).show();
+        Double message = intent.getDoubleExtra(MainActivity.EXTRA_MESSAGE1, 0);
 
+        Toast.makeText(this, String.valueOf(message), Toast.LENGTH_SHORT).show();
     }
-
-    // Obsluga aparatu
-
-        public void addPhoto (View view){
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-
-            if (extras != null) {
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                imageButton.setImageBitmap(imageBitmap);
-            }
-        }
+        switch (requestCode) {
+            case ACTION_TAKE_PHOTO_B: {
+                if (resultCode == RESULT_OK) {
+                    handleBigCameraPhoto();
+                }
+                break;
+            } // ACTION_TAKE_PHOTO_B
+            case ACTION_TAKE_PHOTO_S: {
+                if (resultCode == RESULT_OK) {
+                    handleSmallCameraPhoto(data);
+                }
+                break;
+            } // ACTION_TAKE_PHOTO_S
+        } // switch
+    }
+    // Some lifecycle callbacks so that the image can survive orientation change
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
+        outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
+        super.onSaveInstanceState(outState);
     }
 
-    // Obsluga lokalizacji
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+
+
+
+        //mImageView.setScaleType(ImageView.ScaleType.CENTER);
+        // mImageView.setAdjustViewBounds(true);
+        //  mImageView.getLayoutParams().height = mImageBitmap.getHeight();//set appropriate sizes
+        // mImageView.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;//set appropriate sizes
+        // mImageView.requestLayout();
+        mImageView.setImageBitmap(mImageBitmap);
+
+    }
+
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    private void setBtnListenerOrDisable(
+            ImageButton btn,
+            ImageButton.OnClickListener onClickListener,
+            String intentName
+    ) {
+        if (isIntentAvailable(this, intentName)) {
+            btn.setOnClickListener(onClickListener);
+        } else {
+            btn.setClickable(false);
+        }
+    }
 
         public void addNewPlayground (View view){
             Playground playground = new Playground(42.0, 17.0,
                     String.valueOf(playGroundName.getText()),
-                    String.valueOf(playGroundComment.getText()),
+                    "ghvhg",
                     rate.getRating());
         }
 }
---
+++ app/src/main/java/com/tpanpm/wwsis/placzabaw/AlbumStorageDirFactory.java
@@ -0,0 +1,7 @@
+package com.tpanpm.wwsis.placzabaw;
+
+        import java.io.File;
+
+public abstract class AlbumStorageDirFactory {
+    public abstract File getAlbumStorageDir(String albumName);
+}
