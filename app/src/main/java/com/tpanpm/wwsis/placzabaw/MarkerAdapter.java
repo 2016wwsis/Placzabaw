5e5b8ee86596fa52ceab37f8dae4b69086f80424
Robert <robertman84@gmail.com>
2017-06-24 09:56:01 +0200

    Korekta MarkerAdapter Robert

--- app/src/main/java/com/tpanpm/wwsis/placzabaw/MarkerAdapter.java
+++ app/src/main/java/com/tpanpm/wwsis/placzabaw/MarkerAdapter.java
@@ -10,14 +10,20 @@ import java.util.ArrayList;
 
 public class MarkerAdapter  implements InfoWindowAdapter {
     LayoutInflater inflater = null;
-    float rate = 0;
+    float rate;
     private TextView textViewTitle;
     private TextView textViewDescription;
-
 
     public MarkerAdapter(LayoutInflater inflater) {
         this.inflater = inflater;
     }
+
+    public MarkerAdapter(LayoutInflater inflater, float rate) {
+        this.inflater = inflater;
+        this.rate = rate;
+    }
+
+
 
     public void setRate(float rate){
         this.rate = rate;
@@ -28,15 +34,19 @@ public class MarkerAdapter  implements InfoWindowAdapter {
 
         View v = inflater.inflate(R.layout.custom_info_contents, null);
         if (marker != null) {
+
             textViewTitle = (TextView) v.findViewById(R.id.title_test);
-            textViewTitle.setText(marker.getTitle()+marker.getId());
             textViewDescription = (TextView) v.findViewById(R.id.snippet);
-            textViewDescription.setText(marker.getSnippet());
             final TextView textViewRating = ((TextView) v.findViewById(R.id.rating));
-            textViewRating.setText(String.format("%.1f", rate));
             final RatingBar ratingBar = ((RatingBar) v.findViewById(R.id.rating_bar));
+
+            textViewTitle.setText(marker.getTitle());
+            textViewDescription.setText(marker.getSnippet());
+            textViewRating.setText(String.format("%.1f", rate));
             ratingBar.setRating(rate);
+
         }
+
         return (v);
 
 
