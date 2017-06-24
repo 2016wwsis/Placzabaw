9fac4c57105e47ce45531b43830bf936583d08c1
Robert <robertman84@gmail.com>
2017-06-24 09:34:12 +0200

    Korekta MarkerAdapter Robert

--- app/src/main/java/com/tpanpm/wwsis/placzabaw/MarkerAdapter.java
+++ app/src/main/java/com/tpanpm/wwsis/placzabaw/MarkerAdapter.java
@@ -9,44 +9,44 @@ import com.google.android.gms.maps.model.Marker;
 import java.util.ArrayList;
 
 public class MarkerAdapter  implements InfoWindowAdapter {
-        LayoutInflater inflater = null;
-        float rate = 0;
-        private TextView textViewTitle;
-        private TextView textViewDescription;
-     
+    LayoutInflater inflater = null;
+    float rate = 0;
+    private TextView textViewTitle;
+    private TextView textViewDescription;
 
-        public MarkerAdapter(LayoutInflater inflater) {
-            this.inflater = inflater;
+
+    public MarkerAdapter(LayoutInflater inflater) {
+        this.inflater = inflater;
+    }
+
+    public void setRate(float rate){
+        this.rate = rate;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+
+        View v = inflater.inflate(R.layout.custom_info_contents, null);
+        if (marker != null) {
+            textViewTitle = (TextView) v.findViewById(R.id.title_test);
+            textViewTitle.setText(marker.getTitle()+marker.getId());
+            textViewDescription = (TextView) v.findViewById(R.id.snippet);
+            textViewDescription.setText(marker.getSnippet());
+            final TextView textViewRating = ((TextView) v.findViewById(R.id.rating));
+            textViewRating.setText(String.format("%.1f", rate));
+            final RatingBar ratingBar = ((RatingBar) v.findViewById(R.id.rating_bar));
+            ratingBar.setRating(rate);
         }
-
-        public void setRate(float rate){
-            this.rate = rate;
-        }
-
-        @Override
-        public View getInfoWindow(Marker marker) {
-
-            View v = inflater.inflate(R.layout.custom_info_contents, null);
-            if (marker != null) {
-                textViewTitle = (TextView) v.findViewById(R.id.title_test);
-                textViewTitle.setText(marker.getTitle()+marker.getId());
-                textViewDescription = (TextView) v.findViewById(R.id.snippet);
-                textViewDescription.setText(marker.getSnippet());
-                final TextView textViewRating = ((TextView) v.findViewById(R.id.rating));
-                textViewRating.setText(String.format("%.1f", rate));
-                final RatingBar ratingBar = ((RatingBar) v.findViewById(R.id.rating_bar));
-                ratingBar.setRating(rate);
-            }
-            return (v);
+        return (v);
 
 
-        }
+    }
 
 
     @Override
-        public View getInfoContents(Marker marker) {
-            return null;
-        }
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
 
 
 
